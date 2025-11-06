e060bef56d2b4d5bfb5f783a292f4237a07ef3b1
Dawid Trojanowski <exea.centrum@gmail.com>
2025-11-06 18:24:42 +0100

    v26

--- app/main.py
+++ app/main.py
@@ -12,8 +12,10 @@ from typing import List, Dict, Any
 import time
 import json
 
+
 # Kafka imports
 from kafka import KafkaProducer
+
 
 # OpenTelemetry imports
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
